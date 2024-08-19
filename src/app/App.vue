52cdce640866e4ec332cecf9506c074f6d456d39
number <dekukun756@gmail.com>
2024-08-19 12:16:33 +0500

    init

---
+++ src/app/App.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import { MainPage } from "../pages/main";
+</script>
+
+<template>
+    <MainPage />
+</template>
---
+++ src/app/index.ts
@@ -0,0 +1,6 @@
+import { createApp } from "vue";
+
+import App from "./App.vue";
+import "./index.css";
+
+export const app = createApp(App);
